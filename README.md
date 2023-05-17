ecb5cc446fc27ad893af9b72eaa4060855ccea8f
achiengcatherine <achiengcatherine431@gmail.com>
2023-05-17 17:27:21 +0300

    upload flexboxcontent

---
+++ README.md
@@ -0,0 +1,23 @@
+##CSS FLEXBOX
+What's this? Flexbox is a css box model to easily layout, align and distribute space among items withing a container either horizontally or vertically.
+
+What Flexbox can do for us?
+
+Simplify layouts
+Reduce need for media queries(responsive design)
+
+###Before get started we need to know some of the flexbox basics:
+In flexbox, the element or container which wraps all the children elements or items is called flexbox container and childrens or each children elements/items are called flexbox items.
+
+Flex container has their own properties and values
+
+Same goes to children, also flex items/childrens has their own properties and values
+
+In flexbox, there are two axis, they are Main axis and Cross axis.
+
+To make a flex container display: flex;
+
+By default, flex-direction is row. We can change the flex-direction to
+
+flex-direction: row || row-reverse || column || column-reverse
+<img src="./images/screenshot.png" alt="flexbox"/>
---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import './App.css'
+import kate from './img/kate.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flexbox">
+        <div className="identity">
+          <img src={kate} className="kate" alt="kate" />
+          <h4>Report for</h4>
+          <h2>
+            Catherine <br></br>Onyango
+          </h2>
+          <div />
+          <div className="reports">
+            <li>Daily</li>
+            <li>Weekly</li>
+            <li>Monthly</li>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className="box">
+          <div className="color1"></div>
+          <div className="menu">
+            <div className="wrk">Work</div>
+            <div className="dot">
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </div>
+          </div>
+          <h1>32hrs</h1>
+          <h4>Last Week-36hrs</h4>
+        </div>
+
+        <div className="box">
+          <div className="color2"></div>
+          <div className="menu">
+            <div className="wrk">Exercises</div>
+            <div className="dot2">
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </div>
+          </div>
+          <h1>4hrs</h1>
+          <h4>Last Week-5hrs</h4>
+        </div>
+      </div>
+
+      <div className="">
+        <div className="box">
+          <div className="color3"></div>
+          <div className="menu">
+            <div className="wrk">Play</div>
+            <div className="dot">
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </div>
+          </div>
+          <h1>10hrs</h1>
+          <h4>Last Week-8hrs</h4>
+        </div>
+
+        <div className="box">
+          <div className="color4"></div>
+          <div className="menu">
+            <div className="wrk">Social</div>
+            <div className="dot2">
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </div>
+          </div>
+          <h1>5hrs</h1>
+          <h4>Last Week-10hrs</h4>
+        </div>
+      </div>
+
+      <div className="">
+        <div className="box">
+          <div className="color5"></div>
+          <div className="menu">
+            <div className="wrk">study</div>
+            <div className="dot">
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </div>
+          </div>
+          <h1>4hrs</h1>
+          <h4>Last Week-7hrs</h4>
+        </div>
+
+        <div className="box">
+          <div className="color6"></div>
+          <div className="menu">
+            <div className="wrk">Self Care</div>
+            <div className="dot2">
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </div>
+          </div>
+          <h1>2hrs</h1>
+          <h4>Last Week-2hrs</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
+
+
